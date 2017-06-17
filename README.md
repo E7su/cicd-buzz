4fae1fe8344ee90a5aadd830d8726535480e40da
Etsu <etsu4296@gmail.com>
2017-06-17 14:53:35 +0300

    add attractive landing page #1

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Build Status](https://travis-ci.org/E7su/cicd-buzz.svg?branch=master)](https://travis-ci.org/E7su/cicd-buzz)
+[![BCH compliance](https://bettercodehub.com/edge/badge/E7su/cicd-buzz?branch=master)](https://bettercodehub.com/)
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import os
 import signal
 from flask import Flask
@@ -8,13 +10,39 @@ app = Flask(__name__)
 signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 
-@app.route("/")
+@app.route('/')
 def generate_buzz():
-    page = '<html><body><h1>'
+    page = \
+        '''<html>
+              <title>CI/CD buzz generator</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1.0">
+              <link href="https://fonts.googleapis.com/css?family=Lobster"rel="stylesheet">
+              <style>
+                  body {
+                      padding: 0;
+                      background: url('/static/images/background.jpg') no-repeat;
+                      background-size: cover; 
+                      background-color: #7a7987;
+                      background-blend-mode: multiply;
+                  }.buzz {
+                       color: #fff;
+                       font-family: 'Lobster', cursive;
+                       font-size: 48px;
+                  }
+
+                  div.buzz {
+                      max-width: 50%;
+                      margin: 0 auto;
+                      text-align: center;
+                      position: relative;
+                      top: 25%;
+                  }
+              </style>'''
+
+    page += '<body><div class="buzz"><p class="buzz">'
     page += generator.generate_buzz()
-    page += '</h1></body></html>'
+    page += '</p></div></body></html>'
     return page
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.getenv('PORT'))  # port 5000 is the default
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT'))  # port 5000 is the default		
